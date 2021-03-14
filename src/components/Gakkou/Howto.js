75d23cc0ef468010d07666f055e75e44610a3e1b
jessica-au <AU.JESSICA.M@GMAIL.COM>
2021-03-13 23:50:00 -0800

    styling and howto component build out

--- src/components/Gakkou/Howto.js
+++ src/components/Gakkou/Howto.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'; 
 import Hiragana from './Hiragana';
 import axios from 'axios';
@@ -6,14 +6,29 @@ import axios from 'axios';
 const { REACT_APP_BACKEND_URL } = process.env;
 
 
+
 const Howto = () => {
+
+    const [allHira, setAllHira] = useState([])
+
+    useEffect(() => {
+        const fetchHira = async (req, res) => {
+            const response = await axios.get(`${REACT_APP_BACKEND_URL}/hira/allHira`)
+            const data = await response.data
+            setAllHira(data)
+        }
+        fetchHira();
+
+    }, [])
+
+
     return (
-        <div className="howto">
+        <div className="gakkou">
             <h1>Hiragana Chart</h1>
             
             <div className="howto">
             <Link to="/gakkou/howto">
-                <button type="button" className="hiraButton">
+                <button type="button" className="hiraButton" type="button">
                    Click to expand
                 </button>
             </Link>
