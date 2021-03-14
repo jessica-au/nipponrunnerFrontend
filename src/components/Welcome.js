34d1ed5d71f092445b76e726847f9283ac5b600f
jessica-au <AU.JESSICA.M@GMAIL.COM>
2021-03-14 16:24:54 -0700

    update styling on gakkou.css, moved from index.css

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,9 @@ const Welcome = () => {
     console.log(`>>>> inside Welcome component`)
     return (
         <div className="information">
-            <h1> Welcome </h1>
+           <h1>Welcome</h1>
+            
+            <div className="welcome">At NipponRunner, we provide you with an engaging way to learn your hiragana and master the basics to begin your journey learning the Japanese language smoothly. </div>
         </div>
     )
 }
