85602bb6adeb47f75aa2b6a1dfb1b6ab30e7e92e
jessica-au <AU.JESSICA.M@GMAIL.COM>
2021-03-15 00:19:26 -0700

    styling welcome and about pages for consistency

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,7 @@ const About = () => {
     console.log(`>>>> inside About component`)
     return (
         <div className="about">
-            <h1>About Page</h1>
-            <h3>Rendering about page</h3>
+            <h1>About NipponRunner</h1>
             <p>At NipponRunner, we provide you with an engaging way to learn your hiragana and master the basics to begin your journey learning the Japanese language smoothly. </p>
         </div>
     )
